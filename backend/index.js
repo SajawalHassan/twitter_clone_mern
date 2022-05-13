d3f16d97f26eee49066ebe2c2823a960df12f3b6
sajawalhassan <scientificninja1f12@gmail.com>
2022-05-13 20:36:25 +0500

    connect to db

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const authRouter = require("./routes/auth");
+
+require("dotenv").config();
+
+// Initializing app
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
+
+// Routes middleware
+app.use("/auth", authRouter);
+
+// Connecting to db
+mongoose.connect(process.env.DB_ACCESS_KEY, () =>
+  console.log("Connected to database")
+);
+
+// Creating server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
