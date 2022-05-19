51a0a58a1a4abb1b4e64bb8b40053eea43f97073
sajawalhassan <scientificninja1f12@gmail.com>
2022-05-19 18:41:30 +0500

    Add ability to edit posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -29,4 +29,29 @@ router.post("/create", authenticateToken, async (req, res) => {
   }
 });
 
+router.put("/edit/:id", authenticateToken, async (req, res) => {
+  try {
+    // Finding post
+    const post = await Post.findById(req.params.id);
+
+    if (post.ownerId !== req.user._id) {
+      return res.status(400).json("You're not the owner of this post");
+    }
+
+    // Validating info
+    const { error } = postsValidation(req.body);
+    if (error) return res.status(400).json(error.details[0].message);
+    if ((req.body.title == null) & (req.body.picture == null)) {
+      return res.status(400).json("Fill one of the fields");
+    }
+
+    // Editing post
+    await post.updateOne({ $set: req.body });
+
+    res.json("Post has been updated!");
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
 module.exports = router;
