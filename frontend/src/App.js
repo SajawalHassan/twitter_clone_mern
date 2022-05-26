94d128456a80537c6b7f592dab08fc3e346e06f5
sajawalhassan <scientificninja1f12@gmail.com>
2022-05-26 16:30:41 +0500

    add redux

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
